45c2937f6a9fb2408ffca50c399f39ad8cefb929
Laurent Nizeyimana <trevor@Laurents-MacBook-Pro.local>
2018-03-01 15:22:54 -0500

    Implémentation des boutons et badges de likes et dislikes a l'aide de javascript. Ajout des fichier pro.js, semi.js et ama.js

---
+++ web/semi.js
@@ -0,0 +1,128 @@
+var oneLikeBadge = document.getElementById("onebl");
+var oneDislikeBadge = document.getElementById("onebd");
+var oneLikeButton = document.getElementById("onecl");
+var oneDislikeButton = document.getElementById("onecd");
+var oneLikeCount = 0;
+var oneDislikeCount = 0;
+
+var twoLikeBadge = document.getElementById("twobl");
+var twoDislikeBadge = document.getElementById("twobd");
+var twoLikeButton = document.getElementById("twocl");
+var twoDislikeButton = document.getElementById("twocd");
+var twoLikeCount = 0;
+var twoDislikeCount = 0;
+
+var threeLikeBadge = document.getElementById("threebl");
+var threeDislikeBadge = document.getElementById("threebd");
+var threeLikeButton = document.getElementById("threecl");
+var threeDislikeButton = document.getElementById("threecd");
+var threeLikeCount = 0;
+var threeDislikeCount = 0;
+
+var fourLikeBadge = document.getElementById("fourbl");
+var fourDislikeBadge = document.getElementById("fourbd");
+var fourLikeButton = document.getElementById("fourcl");
+var fourDislikeButton = document.getElementById("fourcd");
+var fourLikeCount = 0;
+var fourDislikeCount = 0;
+
+var fiveLikeBadge = document.getElementById("fivebl");
+var fiveDislikeBadge = document.getElementById("fivebd");
+var fiveLikeButton = document.getElementById("fivecl");
+var fiveDislikeButton = document.getElementById("fivecd");
+var fiveLikeCount = 0;
+var fiveDislikeCount = 0;
+
+var sixLikeBadge = document.getElementById("sixbl");
+var sixDislikeBadge = document.getElementById("sixbd");
+var sixLikeButton = document.getElementById("sixcl");
+var sixDislikeButton = document.getElementById("sixcd");
+var sixLikeCount = 0;
+var sixDislikeCount = 0;
+
+var sevenLikeBadge = document.getElementById("sevenbl");
+var sevenDislikeBadge = document.getElementById("sevenbd");
+var sevenLikeButton = document.getElementById("sevencl");
+var sevenDislikeButton = document.getElementById("sevencd");
+var sevenLikeCount = 0;
+var sevenDislikeCount = 0;
+
+oneLikeButton.onclick = function() {
+	oneLikeCount += 1;
+	oneLikeBadge.innerHTML = oneLikeCount.toString();
+};
+
+oneDislikeButton.onclick = function() {
+	oneDislikeCount += 1;
+	oneDislikeBadge.innerHTML = oneDislikeCount.toString();
+};
+
+twoLikeButton.onclick = function() {
+	twoLikeCount += 1;
+	twoLikeBadge.innerHTML = twoLikeCount.toString();
+};
+
+twoDislikeButton.onclick = function() {
+	twoDislikeCount += 1;
+	twoDislikeBadge.innerHTML = twoDislikeCount.toString();
+};
+
+threeLikeButton.onclick = function() {
+	threeLikeCount += 1;
+	threeLikeBadge.innerHTML = threeLikeCount.toString();
+};
+
+threeDislikeButton.onclick = function() {
+	threeDislikeCount += 1;
+	threeDislikeBadge.innerHTML = threeDislikeCount.toString();
+};
+
+fourLikeButton.onclick = function() {
+	fourLikeCount += 1;
+	fourLikeBadge.innerHTML = fourLikeCount.toString();
+};
+
+fourDislikeButton.onclick = function() {
+	fourDislikeCount += 1;
+	fourDislikeBadge.innerHTML = fourDislikeCount.toString();
+};
+
+fiveLikeButton.onclick = function() {
+	fiveLikeCount += 1;
+	fiveLikeBadge.innerHTML = fiveLikeCount.toString();
+};
+
+fiveDislikeButton.onclick = function() {
+	fiveDislikeCount += 1;
+	fiveDislikeBadge.innerHTML = fiveDislikeCount.toString();
+};
+
+sixLikeButton.onclick = function() {
+	sixLikeCount += 1;
+	sixLikeBadge.innerHTML = sixLikeCount.toString();
+};
+
+sixDislikeButton.onclick = function() {
+	sixDislikeCount += 1;
+	sixDislikeBadge.innerHTML = sixDislikeCount.toString();
+};
+
+sevenLikeButton.onclick = function() {
+	sevenLikeCount += 1;
+	sevenLikeBadge.innerHTML = sevenLikeCount.toString();
+};
+
+sevenDislikeButton.onclick = function() {
+	sevenDislikeCount += 1;
+	sevenDislikeBadge.innerHTML = sevenDislikeCount.toString();
+};
+
+oneLikeButton.onclick = function() {
+	oneLikeCount += 1;
+	oneLikeBadge.innerHTML = oneLikeCount.toString();
+};
+
+oneDislikeButton.onclick = function() {
+	oneDislikeCount += 1;
+	oneDislikeBadge.innerHTML = oneDislikeCount.toString();
+};
