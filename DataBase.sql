be1f31d568d09548026d8564426f274c2726426f
Laurent Nizeyimana <trevor@Laurents-MacBook-Pro.local>
2018-03-20 13:44:35 -0400

    ajout de la table camera dans la base de données.

--- DataBase.sql
+++ DataBase.sql
@@ -12,5 +12,13 @@ CREATE TABLE IF NOT EXISTS DataBase.account (
 		nameAccount	VARCHAR(10)		NOT NULL,
 		mailAccount	VARCHAR(50) 		NOT NULL,
 		mdpAccount	VARCHAR(10)		NOT NULL,
+		loged		boolean				,
 	PRIMARY KEY (mailAccount));
+
+CREATE TABLE IF NOT EXISTS DataBase.camera (
+		idproduct	VARCHAR(9)		NOT NULL,
+		likes		int 				,
+		dislikes	int				,
+		rented		boolean				,
+	PRIMARY KEY (idproduct));
 
